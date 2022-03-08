0ea045e04ca156364295c28c6bf6aee34044d48e
robertfoerster <foersteroberthh@gmail.com>
2022-03-09 00:25:42 +0100

    github link, cam shadow, layout, icon

--- outsmarted_app/lib/main.dart
+++ outsmarted_app/lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'dart:async';
 import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
 
@@ -41,65 +42,84 @@ class MyAppState extends State<MyApp> {
   }
 
   @override
-  void dispose() {
-    _controller.dispose();
+  void dispose() async {
+    await _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     Widget cameraPreview = Container(
-        child: GestureDetector(
-            onTap: () async {
-              try {
-                await _initializeControllerFuture;
-                final image = await _controller.takePicture();
-                var request = http.MultipartRequest(
-                    "POST", Uri.parse("http://192.168.1.2:5000/"));
-                request.files.add(await http.MultipartFile.fromPath(
-                    "image", image.path,
-                    contentType: MediaType("image", "jpeg")));
-                var response = await request.send();
-              } catch (e) {
-                print('test :)');
-              }
-            },
-            child: FutureBuilder<void>(
-              future: _initializeControllerFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  var size = MediaQuery.of(context).size.width * 0.75;
-                  return Center(
-                    child: Container(
-                      width: size,
-                      height: size,
-                      child: ClipRect(
-                        child: OverflowBox(
-                          alignment: Alignment.center,
-                          child: FittedBox(
-                            fit: BoxFit.fitWidth,
-                            child: Container(
-                              width: size,
-                              height: size * _controller.value.aspectRatio,
-                              child: CameraPreview(_controller),
-                            ),
-                          ),
+      child: GestureDetector(
+        onTap: () async {
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller.takePicture();
+            var request = http.MultipartRequest(
+                "POST", Uri.parse("http://192.168.1.2:5000/"));
+            request.files.add(await http.MultipartFile.fromPath(
+                "image", image.path,
+                contentType: MediaType("image", "jpeg")));
+            var response = await request.send();
+          } catch (e) {
+            print('test :)');
+          }
+        },
+        child: FutureBuilder<void>(
+          future: _initializeControllerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              var size = MediaQuery.of(context).size.width * 0.75;
+              return Center(
+                child: Container(
+                  width: size,
+                  height: size,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: ClipRect(
+                    child: OverflowBox(
+                      alignment: Alignment.center,
+                      child: FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Container(
+                          width: size,
+                          height: size * _controller.value.aspectRatio,
+                          child: CameraPreview(_controller),
                         ),
                       ),
-                    )
-                  );
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              },
-            )
-          )
-        );
+                    ),
+                  ),
+                )
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        )
+      )
+    );
 
     return MaterialApp(
       title: 'OutSmarted',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(233, 233, 242, 255),
+      ),
       home: Scaffold(
-        appBar: AppBar(title: const Text('OutSmarted')),
+        appBar: AppBar(
+          leading: const Icon(
+            Icons.camera_alt,
+            color: Color.fromARGB(233, 122, 168, 231),
+          ),
+          title: const Text('OutSmarted')
+          ),
         body: Column(
           children: [
             Container(
@@ -108,9 +128,25 @@ class MyAppState extends State<MyApp> {
             ),
             cameraPreview,
             Container(
-              margin: const EdgeInsets.fromLTRB(0, 32, 0, 0),
-              width: 160,
+              margin: const EdgeInsets.fromLTRB(0, 14, 0, 0),
+              width: 300,
               child: Image.asset('assets/images/tictactoe.png'),
+            ),
+            Center(
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                child: InkWell(
+                  child: const Text(
+                    'GitHub',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      decorationColor: Color.fromARGB(255, 96, 102, 116),
+                      color: Color.fromARGB(255, 96, 102, 116),
+                    ),
+                  ),
+                  onTap: () => launch('https://github.com/foersterrobert/OutSmarted'),
+                ),
+              )
             ),
           ],
         ),
