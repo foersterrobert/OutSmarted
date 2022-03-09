c9e31ff133d0eb58b9b2dd5ac50e6a385964f2a9
robertfoerster <foersteroberthh@gmail.com>
2022-03-09 21:57:14 +0100

    stack grid, error notification, rounded camera

--- outsmarted_app/lib/main.dart
+++ outsmarted_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'dart:async';
+import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
@@ -17,7 +18,7 @@ Future<void> main() async {
       child: MaterialApp(
         title: 'OutSmarted',
         theme: ThemeData(
-          scaffoldBackgroundColor: Color.fromARGB(233, 233, 242, 255),
+          scaffoldBackgroundColor: const Color.fromARGB(233, 233, 242, 255),
         ),
         home: MyApp(
           camera: firstCamera,
@@ -41,9 +42,10 @@ class MyApp extends StatefulWidget {
   MyAppState createState() => MyAppState();
 }
 
-class MyAppState extends State<MyApp> {
+class MyAppState extends State<MyApp> with WidgetsBindingObserver {
   late CameraController _controller;
   late Future<void> _initializeControllerFuture;
+  var _game = 0;
 
   @override
   void initState() {
@@ -56,84 +58,124 @@ class MyAppState extends State<MyApp> {
   }
 
   @override
-  void dispose() async {
-    await _controller.dispose();
+  void dispose() {
+    _controller.dispose();
     super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _initializeControllerFuture = _controller.initialize();
+    }
+  }
+
+  void _changeGame() {
+    setState(() {
+      _game = _game == 0 ? 1 : 0;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size.width * 0.8;
-    
+
     Widget header = Center(
       child: Container(
-        margin: EdgeInsets.fromLTRB(0, 4, 0, 2),
+        margin: const EdgeInsets.fromLTRB(0, 4, 0, 2),
         width: size,
         child: const Text(
-        '🧠 OutSmarted',
-        style: TextStyle(
-          fontSize: 26,
-          fontWeight: FontWeight.bold,
-          color: Colors.black,
+          '🧠 OutSmarted',
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
         ),
-      ),
       ),
     );
 
     Widget cameraPreview = GestureDetector(
-            onTap: () async {
-              try {
-                await _initializeControllerFuture;
-                final image = await _controller.takePicture();
-                var request = http.MultipartRequest(
-                    "POST", Uri.parse("http://192.168.1.2:5000/"));
-                request.files.add(await http.MultipartFile.fromPath(
-                    "image", image.path,
-                    contentType: MediaType("image", "jpeg")));
-                var response = await request.send();
-              } catch (e) {
-                print('error :)');
-              }
-            },
-            child: FutureBuilder<void>(
-              future: _initializeControllerFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return Center(
-                      child: Container(
-                    width: size,
-                    height: size,
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset:
-                              const Offset(0, 2), // changes position of shadow
-                        ),
-                      ],
+        onTap: () async {
+          _changeGame();
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller.takePicture();
+            var request = http.MultipartRequest(
+                "POST", Uri.parse("http://192.168.1.2:5000/"));
+            request.files.add(await http.MultipartFile.fromPath(
+                "image", image.path,
+                contentType: MediaType("image", "jpeg")));
+            var response = await request.send();
+          } catch (e) {
+            showDialog(context: context, builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Error'),
+                content: const Text("Couldn't connect to server!"),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            });
+          }
+        },
+        child: FutureBuilder<void>(
+          future: _initializeControllerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Center(
+                child: Container(
+                  width: size,
+                height: size,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 2), // changes position of shadow
                     ),
-                    child: ClipRect(
-                      child: OverflowBox(
-                        alignment: Alignment.center,
-                        child: FittedBox(
-                          fit: BoxFit.fitWidth,
-                          child: Container(
-                            width: size,
-                            height: size * _controller.value.aspectRatio,
-                            child: CameraPreview(_controller),
-                          ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(4),
+                child: ClipRect(
+                  child: OverflowBox(
+                    alignment: Alignment.center,
+                    child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: SizedBox(
+                        width: size,
+                        height: size * _controller.value.aspectRatio,
+                        child: Stack(
+                          children: <Widget>[
+                            CameraPreview(_controller),
+                            if (_game == 1)
+                              Image.asset(
+                                'assets/images/tictactoeGrid.png',
+                                width: size,
+                                height: size * _controller.value.aspectRatio,
+                              ),
+                          ],
                         ),
                       ),
                     ),
-                  ));
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              },
-            )
-          );
+                  ),
+                ),
+                )
+                )
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+        );
 
     return Scaffold(
       body: ListView(
@@ -142,27 +184,29 @@ class MyAppState extends State<MyApp> {
           cameraPreview,
           Center(
               child: Container(
-            margin: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-            child: Image.asset(
-              'assets/images/tictactoe.png',
-              width: size,
-              height: size,
+              margin: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+              child: Image.asset(
+                'assets/images/tictactoe.png',
+                width: size,
+                height: size,
+              ),
             ),
-          )),
+          ),
           Center(
               child: Container(
-            margin: const EdgeInsets.fromLTRB(0, 12, 0, 6),
-            child: InkWell(
-              child: const Text(
-                '👾 GitHub',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 96, 102, 116),
+              margin: const EdgeInsets.fromLTRB(0, 12, 0, 6),
+              child: InkWell(
+                child: const Text(
+                  '👾 GitHub',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 96, 102, 116),
+                  ),
                 ),
+                onTap: () =>
+                    launch('https://github.com/foersterrobert/OutSmarted'),
               ),
-              onTap: () =>
-                  launch('https://github.com/foersterrobert/OutSmarted'),
             ),
-          )),
+          ),
         ],
       ),
     );
