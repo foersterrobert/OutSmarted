5a201ef64be5fc09d9c506a4b922d8af9ca2f3bb
robertfoerster <foersteroberthh@gmail.com>
2022-03-10 22:16:19 +0100

    structure, new imgs, infoBar

--- outsmarted_app/lib/main.dart
+++ outsmarted_app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'dart:async';
-import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
@@ -85,7 +84,7 @@ class MyAppState extends State<MyApp> with WidgetsBindingObserver {
         margin: const EdgeInsets.fromLTRB(0, 4, 0, 2),
         width: size,
         child: const Text(
-          '🧠 OutSmarted',
+          '🧠 OutSmarted.',
           style: TextStyle(
             fontSize: 26,
             fontWeight: FontWeight.bold,
@@ -95,87 +94,119 @@ class MyAppState extends State<MyApp> with WidgetsBindingObserver {
       ),
     );
 
+    Widget footer = Center(
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(0, 12, 0, 6),
+        child: InkWell(
+          child: const Text(
+            '👾 GitHub',
+            style: TextStyle(
+              fontSize: 14,
+              color: Color.fromARGB(255, 96, 102, 116),
+            ),
+          ),
+          onTap: () => launch('https://github.com/foersterrobert/OutSmarted'),
+        ),
+      ),
+    );
+
     Widget cameraPreview = GestureDetector(
-        onTap: () async {
-          _changeGame();
-          try {
-            await _initializeControllerFuture;
-            final image = await _controller.takePicture();
-            var request = http.MultipartRequest(
-                "POST", Uri.parse("http://192.168.1.2:5000/"));
-            request.files.add(await http.MultipartFile.fromPath(
-                "image", image.path,
-                contentType: MediaType("image", "jpeg")));
-            var response = await request.send();
-          } catch (e) {
-            showDialog(context: context, builder: (BuildContext context) {
-              return AlertDialog(
-                title: const Text('Error'),
-                content: const Text("Couldn't connect to server!"),
-                actions: <Widget>[
-                  TextButton(
-                    child: const Text('OK'),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
-              );
-            });
-          }
-        },
-        child: FutureBuilder<void>(
-          future: _initializeControllerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Center(
-                child: Container(
-                  width: size,
-                height: size,
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: const Offset(0, 2), // changes position of shadow
+      onTap: () async {
+        _changeGame();
+        try {
+          await _initializeControllerFuture;
+          final image = await _controller.takePicture();
+          var request = http.MultipartRequest(
+              "POST", Uri.parse("http://192.168.1.2:5000/"));
+          request.files.add(await http.MultipartFile.fromPath(
+              "image", image.path,
+              contentType: MediaType("image", "jpeg")));
+          var response = await request.send();
+        } catch (e) {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('Error'),
+                  content: const Text("Couldn't connect to server! 😬"),
+                  actions: <Widget>[
+                    TextButton(
+                      child: const Text('OK'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
                     ),
                   ],
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(4),
-                child: ClipRect(
-                  child: OverflowBox(
-                    alignment: Alignment.center,
-                    child: FittedBox(
-                      fit: BoxFit.fitWidth,
-                      child: SizedBox(
-                        width: size,
-                        height: size * _controller.value.aspectRatio,
-                        child: Stack(
-                          children: <Widget>[
-                            CameraPreview(_controller),
-                            if (_game == 1)
-                              Image.asset(
-                                'assets/images/tictactoeGrid.png',
-                                width: size,
-                                height: size * _controller.value.aspectRatio,
+                );
+              });
+        }
+      },
+      child: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Center(
+                child: Container(
+                    width: size,
+                    height: size,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset:
+                              const Offset(0, 2), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(4),
+                      child: ClipRect(
+                        child: OverflowBox(
+                          alignment: Alignment.center,
+                          child: FittedBox(
+                            fit: BoxFit.fitWidth,
+                            child: SizedBox(
+                              width: size,
+                              height: size * _controller.value.aspectRatio,
+                              child: Stack(
+                                children: <Widget>[
+                                  CameraPreview(_controller),
+                                  if (_game == 1)
+                                    Opacity(
+                                      opacity: 0.8,
+                                      child: Image.asset(
+                                        'assets/images/tictactoe/tictactoeGrid.png',
+                                        width: size,
+                                        height: size *
+                                            _controller.value.aspectRatio,
+                                      ),
+                                    )
+                                ],
                               ),
-                          ],
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                ),
-                )
-                )
-              );
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
+                    )));
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+
+    Widget gameVis = Center(
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+        child: Image.asset(
+          'assets/images/tictactoe/tictactoeFull.png',
+          width: size,
+          height: size,
         ),
-        );
+      ),
+    );
 
     return Scaffold(
       body: ListView(
@@ -183,30 +214,21 @@ class MyAppState extends State<MyApp> with WidgetsBindingObserver {
           header,
           cameraPreview,
           Center(
-              child: Container(
+            child: Container(
               margin: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-              child: Image.asset(
-                'assets/images/tictactoe.png',
-                width: size,
-                height: size,
-              ),
-            ),
-          ),
-          Center(
-              child: Container(
-              margin: const EdgeInsets.fromLTRB(0, 12, 0, 6),
-              child: InkWell(
-                child: const Text(
-                  '👾 GitHub',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 96, 102, 116),
-                  ),
+              width: size,
+              child: Text(
+                _game == 0 ? 'No Game detected.' : 'TIC-TAC-TOE',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 96, 102, 116),
                 ),
-                onTap: () =>
-                    launch('https://github.com/foersterrobert/OutSmarted'),
               ),
             ),
           ),
+          gameVis,
+          footer,
         ],
       ),
     );
