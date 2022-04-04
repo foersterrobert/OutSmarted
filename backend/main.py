6522bf684e51b4f080a7e1ddaeec58effcd78bfd
HowToCarryyy <howtobecarried@web.de>
2022-04-04 18:50:25 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ import cv2
 from PIL import Image
 import tictactoe as ttt
 from connectfour import connectFourState
+import math
 
 app = Flask(__name__)
 
@@ -31,13 +32,83 @@ def index():
     print(state)
     return jsonify({'state': state, 'game': game})
 
-def bestmove(field):
+# MINIMAX ALGORITHM
+
+def checkForWin(field):
+
+    if (max(field.sum(axis=1)) == 3 or max(field.sum(axis=0)) == 3):
+        return 10
+    elif (min(field.sum(axis=1)) == -3 or min(field.sum(axis=0)) == -3):
+        return -10
+    elif (np.trace(field) == 3):
+        return 10
+    elif (np.trace(field) == -3):
+        return -10
+    elif (np.trace(np.fliplr(field)) == 3):
+        return 10
+    elif (np.trace(np.fliplr(field)) == -3):
+        return -10
+    elif np.count_nonzero(field) == 9:
+        return 1000
+    
+    return False
+
+
+
+def minimax(field, isMaximizing):
+    if checkForWin(field) == 10:
+        return {
+            'move': None,
+            'score': - 1 * ((9 - np.count_nonzero(field)) + 1)
+        }
+    elif checkForWin(field) == -10:
+        return {
+            'move': None,
+            'score': 1 * ((9 - np.count_nonzero(field)) + 1)
+        }
+    elif checkForWin(field) == 1000:
+        return {
+            'move': None,
+            'score': 0
+        }
+    
+    if isMaximizing:
+        best = {'move': None, 'score': -math.inf}
+    elif not isMaximizing:
+        best = {'move': None, 'score': math.inf}
+
     for i in range(3):
         for j in range(3):
-            if field[i][j] == 0:
-                field[i][j] = -2
-                return field
-    return field
+            if isMaximizing:
+                if field[i][j] == 0:
+                    field[i][j] = -1
+                    sim_score = minimax(field, False)
+                    field[i][j] = 0
+
+                    sim_score['move'] = (i, j)
+                    if sim_score['score'] > best['score']:
+                        best = sim_score
+            elif not isMaximizing:
+                if field[i][j] == 0:
+                    field[i][j] = 1
+                    sim_score = minimax(field, True)
+                    field[i][j] = 0
+
+                    sim_score['move'] = (i, j)
+                    if sim_score['score'] < best['score']:
+                        best = sim_score
+    return best
+
+def bestmove(field):
+    if np.count_nonzero(field) == 0:
+        move = (0,0)
+    else:
+        move = minimax(field, True)['move']
+    print(move)
+    field[move[0]][move[1]] = -2
+
+
+# MINIMAX ALGORITHM
 
 def tictactoeState(image):
     image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
