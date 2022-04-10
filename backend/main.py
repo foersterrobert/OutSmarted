066911691f9f83672c24c7e783d55986d8673a4a
robertfoerster <foersteroberthh@gmail.com>
2022-04-10 11:33:45 +0200

    yolo backend

--- backend/main.py
+++ backend/main.py
@@ -1,36 +1,13 @@
 import numpy as np
 from flask import Flask, jsonify, request
-import cv2
 from PIL import Image
-import tictactoe as ttt
 from connectfour import connectFourState
+import tictactoe as ttt
+import torch
+from PIL import Image
+import numpy as np
+from torchvision.transforms.functional import to_tensor
 import math
-
-app = Flask(__name__)
-
-@app.route("/", methods=['POST'])
-def index():
-    image = request.files['image']
-    image = Image.open(image)
-    width, height = image.size
-    image = image.crop((
-        max(0, (width - height) / 2), 
-        max(0, (height - width) / 2), 
-        width - max(0, (width - height) / 2),
-        height - max(0, (height - width) / 2)
-    ))
-    game = 1 # int(request.form['game'])
-    try:
-        if game == 1:
-            state = tictactoeState(image)
-        elif game == 2:
-            state = connectFourState(image)
-        else:
-            state = []
-    except Exception:
-        state = []
-    print(state)
-    return jsonify({'state': state, 'game': game})
 
 # MINIMAX ALGORITHM START
 def checkForWin(field):
@@ -104,16 +81,92 @@ def bestmove(field):
             field[move[0]][move[1]] = -2
     return field
 
+app = Flask(__name__)
+fieldModel = ttt.FieldModel()
+fieldModel.load_state_dict(torch.load('fieldModel.pth', map_location='cpu'))
+fieldModel.eval()
+boardModel = ttt.BoardModel()
+boardModel.load_state_dict(torch.load('boardModel.pth', map_location='cpu'))
+boardModel.eval()
+
 def tictactoeState(image):
-    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-    resized_original_image = cv2.resize(image, (480, 480))
-    img = ttt.pre_processing(resized_original_image)
-    img = ttt.build_around_to_500(img)
-    contours = ttt.find_contours(img, 3)
-    contours = ttt.move_contours_back_to_480(contours)
-    state = ttt.find_digital_game_array(contours, ttt.recognize_contour_type)
+    image = image.convert('L')
+    imageT = to_tensor(image).reshape(1, 1, 168, 168)
+    out = boardModel(imageT)
+    bboxes = ttt.cellboxes_to_boxes(out)
+    bboxes = ttt.non_max_suppression(bboxes[0], iou_threshold=0.5, threshold=0.4, box_format="midpoint")
+    
+    fieldDict = {
+        '0': torch.zeros((1, 28, 28)),
+        '1': torch.zeros((1, 28, 28)),
+        '2': torch.zeros((1, 28, 28)),
+        '3': torch.zeros((1, 28, 28)),
+        '4': torch.zeros((1, 28, 28)),
+        '5': torch.zeros((1, 28, 28)),
+        '6': torch.zeros((1, 28, 28)),
+        '7': torch.zeros((1, 28, 28)),
+        '8': torch.zeros((1, 28, 28)),
+    }
+
+    confidenceDict = {
+        '0': 0,
+        '1': 0,
+        '2': 0,
+        '3': 0,
+        '4': 0,
+        '5': 0,
+        '6': 0,
+        '7': 0,
+        '8': 0,
+    }
+
+    for box in bboxes:
+        class_idx = box[0]
+        confidence = box[1]
+        if confidence > confidenceDict[str(int(class_idx))]:
+            x = box[2] * 168
+            y = box[3] * 168
+            w = box[4] * 168
+            h = box[5] * 168
+            im1 = image.crop(
+                (x - w / 2, y - h / 2, x + w / 2, y + h / 2)
+            )
+            im1 = im1.resize((28, 28))
+            fieldDict[str(int(class_idx))] = to_tensor(im1)
+
+    fields = torch.stack(list(fieldDict.values()))
+
+    out = fieldModel(fields)
+    state = out.argmax(1).numpy().reshape(3, 3) - 1
     state = bestmove(state)
+
     return state.astype(int).tolist()
+
+@app.route("/state", methods=['POST'])
+def getState():
+    image = request.files['image']
+    image = Image.open(image)
+    width, height = image.size
+    image = image.crop((
+        max(0, (width - height) / 2), 
+        max(0, (height - width) / 2), 
+        width - max(0, (width - height) / 2),
+        height - max(0, (height - width) / 2)
+    ))
+    game = int(request.form['game'])
+    # player = int(request.form['player'])
+    if game == 1:
+        state = tictactoeState(image)
+    elif game == 2:
+        state = connectFourState(image)
+    else:
+        state = []
+    return jsonify({'state': state})
+
+@app.route("/game", methods=['POST'])
+def getGame():
+    game = 1
+    return jsonify({'game': game})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- backend/tictactoe.py
+++ backend/tictactoe.py
@@ -1,237 +1,225 @@
-import cv2
+import torch
+import torch.nn as nn
+import os
+from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from torchvision.transforms.functional import to_tensor
+import torch.nn.functional as F
 
-def pre_processing(img):
-    img = cv2.pyrMeanShiftFiltering(src=img, sp=11, sr=43)
-    img = cv2.GaussianBlur(img, (7, 7), 1)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
-    gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
-    img = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-    kernel = np.ones((5, 5), np.uint8)
-    img = cv2.erode(img, kernel)
-    return img
+class FieldModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.seq = nn.Sequential(
+            self._block(1, 32, 3),
+            self._block(32, 48, 3),
+            self._block(48, 64, 3),
+            self._block(64, 80, 3),
+            self._block(80, 96, 3),
+            self._block(96, 112, 3),
+            self._block(112, 128, 3),
+            self._block(128, 144, 3),
+            self._block(144, 160, 3),
+            self._block(160, 176, 3),
+            Flatten(),
+            nn.Linear(11264, 3, bias=False),
+            nn.BatchNorm1d(3),
+        )
 
-def build_around_to_500(img):
-    new_img = np.ones((500, 500), np.uint8)
-    new_img.fill(255)
-    counter = 0
-    for i in range(len(new_img)):
-        if 10 <= i < 490:
-            new_img[i][10:490] = img[counter]
-            counter += 1
-    return new_img
+    def _block(self, input_dim, output_dim, kernel_size):
+        return nn.Sequential(
+            nn.Conv2d(input_dim, output_dim, kernel_size, bias=False),
+            nn.BatchNorm2d(output_dim),
+            nn.ReLU()
+        )
 
-def find_maxes(data, how_many):
-    tmp_list = data.copy()
-    tmp_list.sort()
-    return tmp_list[-how_many:]
-    
-def find_biggest_contours_indices(contours, how_many):
-    sizes = []
-    indices = []
-    for i in range(len(contours)):
-        sizes.append(cv2.contourArea(contours[i]))
-    maxes = find_maxes(sizes, how_many)
-    for i in range(how_many):
-        indices.append(sizes.index(maxes[i]))
-    return indices
+    def forward(self, x):
+        x = self.seq(x)
+        return F.log_softmax(x, dim=1)
 
-def filter_contours(contour_vector, hierarchy_vector, how_many_biggest_to_ignore):
-    if 0 < how_many_biggest_to_ignore <= len(contour_vector):
-        contours = []
-        indices = find_biggest_contours_indices(contour_vector, how_many_biggest_to_ignore)
-        for i in range(len(contour_vector)):
-            if cv2.contourArea(contour_vector[i]) > 300:
-                if i in indices:
-                    continue
-                elif hierarchy_vector[0][i][3] in indices:
-                    contours.append(contour_vector[i])
-        return contours
-    else:
-        return contour_vector
+class Flatten(nn.Module):
+    def forward(self, x):
+        return torch.flatten(x.permute(0, 2, 3, 1), 1)
 
-def find_contours(img, how_many_biggest_to_ignore):
-    contour_vector, hierarchy_vector = cv2.findContours(image=img, mode=cv2.RETR_TREE,
-                                                           method=cv2.CHAIN_APPROX_SIMPLE)
-    contours = filter_contours(contour_vector, hierarchy_vector, how_many_biggest_to_ignore)
-    return contours
+config = [
+    (28, 3, 1),
+    (64, 3, 2),
+    ["B", 1],
+    (124, 3, 2),
+    ["B", 2],
+    (256, 3, 2),
+    ["B", 6],
+    (256, 3, 2),
+    ["B", 8],
+    (512, 3, 2),
+    ["B", 8],
+    (512, 3, 1),
+    ["B", 4],
+    (256, 2, 1),
+    (96, 3, 1),
+    (19, 3, 1),
+]
 
-def move_contours_back_to_480(contours):
-    for i in range(len(contours)):
-        for j in range(len(contours[i])):
-            contours[i][j][0][0] -= 10
-            contours[i][j][0][1] -= 10
-    return contours
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, bn_act=True, **kwargs):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=not bn_act, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.leaky = nn.LeakyReLU(0.1)
+        self.use_bn_act = bn_act
 
-def recognize_contour_type(contour):
-    poly = get_approx_poly(contour)
-    is_convex = cv2.isContourConvex(poly)
-    contour_type = 0
-    if is_convex:
-        contour_type = 1
-    elif not is_convex:
-        contour_type = -1
-    return contour_type
-
-def get_approx_poly(contour):
-    epsilon = 0.05 * cv2.arcLength(contour, True)
-    return cv2.approxPolyDP(contour, epsilon, True)
-
-def find_digital_game_array(contours, recognize_contour_type):
-    array = np.zeros((3, 3), np.int8)
-    for contour in range(len(contours)):
-        for i in range(3):
-            for j in range(3):
-                contour_inside = get_contour_inside_area(contours[contour], [j * 160, i * 160],
-                                                         [j * 160 + 160, i * 160 + 160])
-                if len(contour_inside) > 0:
-                    original_poly_participation = cv2.contourArea(contour_inside) / cv2.contourArea(contours[contour])
-                    if original_poly_participation > 0.6:
-                        array[i][j] = recognize_contour_type(contours[contour])
-    return array
-
-def get_contour_inside_area(contour, left_top_corner, right_bottom_corner):
-    if check_if_belongs(contour, left_top_corner, right_bottom_corner):
-        new_contour_elements = []
-
-        points_over_top_edge = get_points_over_top_edge(contour, left_top_corner[1])
-        points_below_bottom_edge = get_points_below_bottom_edge(contour, right_bottom_corner[1])
-        points_behind_left_edge = get_points_behind_left_edge(contour, left_top_corner[0])
-        points_behind_right_edge = get_points_behind_right_edge(contour, right_bottom_corner[0])
-
-        left_top_corner_intersection = []
-        if points_over_top_edge and points_behind_left_edge:
-            left_top_corner_intersection = find_intersection(points_over_top_edge, points_behind_left_edge)
-
-        right_top_corner_intersection = []
-        if points_over_top_edge and points_behind_right_edge:
-            right_top_corner_intersection = find_intersection(points_over_top_edge, points_behind_right_edge)
-
-        left_bottom_corner_intersection = []
-        if points_below_bottom_edge and points_behind_left_edge:
-            left_bottom_corner_intersection = find_intersection(points_below_bottom_edge, points_behind_left_edge)
-
-        right_bottom_corner_intersection = []
-        if points_below_bottom_edge and points_behind_right_edge:
-            right_bottom_corner_intersection = find_intersection(points_below_bottom_edge, points_behind_right_edge)
-
-        # LEFT TOP CORNER
-        if left_top_corner_intersection:
-            new_contour_elements.append(left_top_corner)
-            for i in range(len(contour)):
-                if contour[i][0][0] > left_top_corner[0] and contour[i][0][1] > left_top_corner[1]:
-                    new_contour_elements.append([contour[i][0][0], contour[i][0][1]])
-
-        # RIGHT TOP CORNER
-        elif right_top_corner_intersection:
-            new_contour_elements.append([right_bottom_corner[0], left_top_corner[1]])
-            for i in range(len(contour)):
-                if contour[i][0][0] < right_bottom_corner[0] and contour[i][0][1] > left_top_corner[1]:
-                    new_contour_elements.append([contour[i][0][0], contour[i][0][1]])
-
-        # LEFT BOTTOM CORNER
-        elif left_bottom_corner_intersection:
-            new_contour_elements.append([left_top_corner[0], right_bottom_corner[1]])
-            for i in range(len(contour)):
-                if contour[i][0][0] > left_top_corner[0] and contour[i][0][1] < right_bottom_corner[1]:
-                    new_contour_elements.append([contour[i][0][0], contour[i][0][1]])
-
-        # RIGHT BOTTOM CORNER
-        elif right_bottom_corner_intersection:
-            new_contour_elements.append(right_bottom_corner)
-            for i in range(len(contour)):
-                if contour[i][0][0] < right_bottom_corner[0] and contour[i][0][1] < right_bottom_corner[1]:
-                    new_contour_elements.append([contour[i][0][0], contour[i][0][1]])
-
-        # OVER TOP EDGE
-        elif points_over_top_edge:
-            for i in range(len(contour)):
-                tmp = [contour[i][0][0], contour[i][0][1]]
-                if tmp not in points_over_top_edge:
-                    new_contour_elements.append(tmp)
-
-        # BELOW BOTTOM EDGE
-        elif points_below_bottom_edge:
-            for i in range(len(contour)):
-                tmp = [contour[i][0][0], contour[i][0][1]]
-                if tmp not in points_below_bottom_edge:
-                    new_contour_elements.append(tmp)
-
-        # BEHIND LEFT EDGE
-        elif points_behind_left_edge:
-            for i in range(len(contour)):
-                tmp = [contour[i][0][0], contour[i][0][1]]
-                if tmp not in points_behind_left_edge:
-                    new_contour_elements.append(tmp)
-
-        # BEHIND RIGHT EDGE
-        elif points_behind_right_edge:
-            for i in range(len(contour)):
-                tmp = [contour[i][0][0], contour[i][0][1]]
-                if tmp not in points_behind_right_edge:
-                    new_contour_elements.append(tmp)
+    def forward(self, x):
+        if self.use_bn_act:
+            return self.leaky(self.bn(self.conv(x)))
         else:
-            return contour
-            
-        new_contour = np.array(new_contour_elements).reshape((-1, 1, 2)).astype(np.int32)
-        return new_contour
-    else:
-        return []
+            return self.conv(x)
 
-def check_if_belongs(contour, left_top_corner, right_bottom_corner):
-    for i in range(len(contour)):
-        if left_top_corner[0] <= contour[i][0][0] <= right_bottom_corner[0] and left_top_corner[1] <= contour[i][0][1] \
-                <= right_bottom_corner[1]:
-            return True
-    return False
+class ResidualBlock(nn.Module):
+    def __init__(self, channels, use_residual=True, num_repeats=1):
+        super().__init__()
+        self.layers = nn.ModuleList()
+        for repeat in range(num_repeats):
+            self.layers += [
+                nn.Sequential(
+                    CNNBlock(channels, channels // 2, kernel_size=1),
+                    CNNBlock(channels // 2, channels, kernel_size=3, padding=1),
+                )
+            ]
+        self.use_residual = use_residual
+        self.num_repeats = num_repeats
 
-def get_points_over_top_edge(contour, y):
-    if y > 0:
-        points = []
-        for i in range(len(contour)):
-            if contour[i][0][1] < y:
-                tmp = [contour[i][0][0], contour[i][0][1]]
-                points.append(tmp)
-        return points
-    else:
-        return []
+    def forward(self, x):
+        for layer in self.layers:
+            if self.use_residual:
+                x = x + layer(x)
+            else:
+                x = layer(x)
+        return x
 
-def get_points_below_bottom_edge(contour, y):
-    if y < 480:
-        points = []
-        for i in range(len(contour)):
-            if contour[i][0][1] > y:
-                tmp = [contour[i][0][0], contour[i][0][1]]
-                points.append(tmp)
-        return points
-    else:
-        return []
+class BoardModel(nn.Module):
+    def __init__(self, in_channels=1):
+        super().__init__()
+        self.in_channels = in_channels
+        self.layers = self._create_conv_layers()
 
-def get_points_behind_left_edge(contour, x):
-    if x > 0:
-        points = []
-        for i in range(len(contour)):
-            if contour[i][0][0] < x:
-                tmp = [contour[i][0][0], contour[i][0][1]]
-                points.append(tmp)
-        return points
-    else:
-        return []
+    def forward(self, x):
+        for layer in self.layers:
+          x = layer(x)
+        return torch.flatten(x, start_dim=1)
 
-def get_points_behind_right_edge(contour, x):
-    if x < 480:
-        points = []
-        for i in range(len(contour)):
-            if contour[i][0][0] > x:
-                tmp = [contour[i][0][0], contour[i][0][1]]
-                points.append(tmp)
-        return points
-    else:
-        return []
+    def _create_conv_layers(self):
+        layers = nn.ModuleList()
+        in_channels = self.in_channels
 
-def find_intersection(list1, list2):
-    intersectiong_list = []
-    for i in range(len(list1)):
-        if list1[i] in list2:
-            intersectiong_list.append(list1[i])
-    return intersectiong_list
+        for idx, module in enumerate(config):
+            if isinstance(module, tuple):
+                out_channels, kernel_size, stride = module
+                layers.append(
+                    CNNBlock(
+                        in_channels,
+                        out_channels,
+                        kernel_size=kernel_size,
+                        stride=stride,
+                        padding=1 if kernel_size >= 2 else 0,
+                    )
+                )
+                in_channels = out_channels
+
+            elif isinstance(module, list):
+                num_repeats = module[1]
+                layers.append(ResidualBlock(in_channels, num_repeats=num_repeats,))
+        return layers
+
+def intersection_over_union(boxes_preds, boxes_labels, box_format="midpoint"):
+    if box_format == "midpoint":
+        box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
+        box1_y1 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
+        box1_x2 = boxes_preds[..., 0:1] + boxes_preds[..., 2:3] / 2
+        box1_y2 = boxes_preds[..., 1:2] + boxes_preds[..., 3:4] / 2
+        box2_x1 = boxes_labels[..., 0:1] - boxes_labels[..., 2:3] / 2
+        box2_y1 = boxes_labels[..., 1:2] - boxes_labels[..., 3:4] / 2
+        box2_x2 = boxes_labels[..., 0:1] + boxes_labels[..., 2:3] / 2
+        box2_y2 = boxes_labels[..., 1:2] + boxes_labels[..., 3:4] / 2
+
+    if box_format == "corners":
+        box1_x1 = boxes_preds[..., 0:1]
+        box1_y1 = boxes_preds[..., 1:2]
+        box1_x2 = boxes_preds[..., 2:3]
+        box1_y2 = boxes_preds[..., 3:4]  # (N, 1)
+        box2_x1 = boxes_labels[..., 0:1]
+        box2_y1 = boxes_labels[..., 1:2]
+        box2_x2 = boxes_labels[..., 2:3]
+        box2_y2 = boxes_labels[..., 3:4]
+
+    x1 = torch.max(box1_x1, box2_x1)
+    y1 = torch.max(box1_y1, box2_y1)
+    x2 = torch.min(box1_x2, box2_x2)
+    y2 = torch.min(box1_y2, box2_y2)
+
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+
+    box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
+    box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
+
+    return intersection / (box1_area + box2_area - intersection + 1e-6)
+
+def non_max_suppression(bboxes, iou_threshold, threshold, box_format="corners"):
+    assert type(bboxes) == list
+    bboxes = [box for box in bboxes if box[1] > threshold]
+    bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
+    bboxes_after_nms = []
+
+    while bboxes:
+        chosen_box = bboxes.pop(0)
+        bboxes = [
+            box
+            for box in bboxes
+            if box[0] != chosen_box[0]
+            or intersection_over_union(
+                torch.tensor(chosen_box[2:]),
+                torch.tensor(box[2:]),
+                box_format=box_format,
+            )
+            < iou_threshold
+        ]
+        bboxes_after_nms.append(chosen_box)
+    return bboxes_after_nms
+
+def convert_cellboxes(predictions, S=7, C=9):
+    predictions = predictions.to("cpu")
+    batch_size = predictions.shape[0]
+    predictions = predictions.reshape(batch_size, 7, 7, C+10)
+    bboxes1 = predictions[..., C+1:C+5]
+    bboxes2 = predictions[..., C+6:C+10]
+    scores = torch.cat(
+        (predictions[..., C].unsqueeze(0), predictions[..., C+5].unsqueeze(0)), dim=0
+    )
+    best_box = scores.argmax(0).unsqueeze(-1)
+    best_boxes = bboxes1 * (1 - best_box) + best_box * bboxes2
+    cell_indices = torch.arange(S).repeat(batch_size, S, 1).unsqueeze(-1)
+    x = 1 / S * (best_boxes[..., :1] + cell_indices)
+    y = 1 / S * (best_boxes[..., 1:2] + cell_indices.permute(0, 2, 1, 3))
+    w_y = 1 / S * best_boxes[..., 2:4]
+    converted_bboxes = torch.cat((x, y, w_y), dim=-1)
+    predicted_class = predictions[..., :C].argmax(-1).unsqueeze(-1)
+    best_confidence = torch.max(predictions[..., C], predictions[..., C+5]).unsqueeze(
+        -1
+    )
+    converted_preds = torch.cat(
+        (predicted_class, best_confidence, converted_bboxes), dim=-1
+    )
+    return converted_preds
+    
+def cellboxes_to_boxes(out, S=7, C=9):
+    converted_pred = convert_cellboxes(out).reshape(out.shape[0], S * S, -1)
+    converted_pred[..., 0] = converted_pred[..., 0].long()
+    all_bboxes = []
+
+    for ex_idx in range(out.shape[0]):
+        bboxes = []
+
+        for bbox_idx in range(S * S):
+            bboxes.append([x.item() for x in converted_pred[ex_idx, bbox_idx, :]])
+        all_bboxes.append(bboxes)
+    return all_bboxes
