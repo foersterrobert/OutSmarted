d5923655e59561eb53573b97147ee1be2748f8b1
robertfoerster <foersteroberthh@gmail.com>
2022-05-16 10:31:43 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,7 @@ def connectfourState(image, player):
     if col:
         if cfMove.is_valid_location(state, col):
             row = cfMove.get_next_open_row(state, col)
-            cfMove.drop_piece(state, row, col, player*2)
+            cfMove.drop_piece(state, row, col, player*-2)
     state *= player
     state = np.flip(state, 0)
     return state.astype(int).tolist()
