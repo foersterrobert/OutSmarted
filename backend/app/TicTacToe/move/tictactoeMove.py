ef1bd8884fca2cb755b14d109978fbd213eac751
simonlsr <89278229+simonlsr@users.noreply.github.com>
2022-10-07 13:57:40 +0200

    value 0 to 1

--- backend/app/TicTacToe/move/tictactoeMove.py
+++ backend/app/TicTacToe/move/tictactoeMove.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import math
 # MINIMAX ALGORITHM
@@ -19,40 +18,44 @@ def checkForWin(field):
         return 1000
     return False
 
-def minimax(field, isMaximizing, alpha, beta, probability):
+def minimax(field, isMaximizing, alpha, beta, probability, counter):
+    counter += 1
     if checkForWin(field) == 10:
-        probability[0] += 1
+        probability += 1
         return {
             'move': None,
             'score': - 1 * ((9 - np.count_nonzero(field)) + 1),
-            'winning_probability': probability
+            'winning_probability': probability,
+            'counter': counter
         }
     elif checkForWin(field) == -10:
-        probability[1] += 1
+        probability += 0
         return {
             'move': None,
             'score': 1 * ((9 - np.count_nonzero(field)) + 1),
-            'winning_probability': probability
+            'winning_probability': probability,
+            'counter': counter
         }
     elif checkForWin(field) == 1000:
-        probability[2] += 1
+        probability += 0.5
         return {
             'move': None,
             'score': 0,
-            'winning_probability': probability
+            'winning_probability': probability,
+            'counter': counter
         }
     
     if isMaximizing:
-        best = {'move': None, 'score': -math.inf, 'winning_probability': probability}
+        best = {'move': None, 'score': -math.inf, 'winning_probability': probability, 'counter': counter}
     elif not isMaximizing:
-        best = {'move': None, 'score': math.inf, 'winning_probability': probability}
+        best = {'move': None, 'score': math.inf, 'winning_probability': probability, 'counter': counter}
 
     for i in range(3):
         for j in range(3):
             if isMaximizing:
                 if field[i][j] == 0:
                     field[i][j] = -1
-                    sim_score = minimax(field, False, alpha, beta, probability)
+                    sim_score = minimax(field, False, alpha, beta, probability, counter)
                     field[i][j] = 0
 
                     sim_score['move'] = (i, j)
@@ -64,7 +67,7 @@ def minimax(field, isMaximizing, alpha, beta, probability):
             elif not isMaximizing:
                 if field[i][j] == 0:
                     field[i][j] = 1
-                    sim_score = minimax(field, True, alpha, beta, probability)
+                    sim_score = minimax(field, True, alpha, beta, probability, counter)
                     field[i][j] = 0
 
                     sim_score['move'] = (i, j)
@@ -74,16 +77,18 @@ def minimax(field, isMaximizing, alpha, beta, probability):
                         break
     return best
 def bestMove(field):
-    winning_probability = [0, 0, 0]
+    winning_probability = 0
+    counter = 0
     if np.count_nonzero(field) != 9:
         if np.count_nonzero(field) == 0:
             move = (0,0)
         else:
-            move = minimax(field, True, -math.inf, math.inf, winning_probability)
+            move = minimax(field, True, -math.inf, math.inf, winning_probability, counter)
+            counter = move['counter']
             winning_probability = move["winning_probability"]
             move = move["move"]
         if move != None:
             field[move[0]][move[1]] = -2
     # winning_probality = [win, lose, draw]
-    winning_probability = [round(x / sum(winning_probability), 2) if sum(winning_probability) != 0 else x for x in winning_probability]
+    winning_probability = [winning_probability / counter if counter != 0 else 0][0]
     return field, winning_probability
